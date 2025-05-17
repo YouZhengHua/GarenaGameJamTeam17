e2aef82bfc5e06c482723ba7c4f8c349f3b8ecd5
YouZhengHua <42639936+YouZhengHua@users.noreply.github.com>
2025-05-17 15:37:52 +0800

    feat: 更新節拍內容

--- src/GarenaGameJam2025Team17/Assets/FunGamer/Scripts/AudioController.cs
+++ src/GarenaGameJam2025Team17/Assets/FunGamer/Scripts/AudioController.cs
@@ -1,38 +1,111 @@
+using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class AudioController : MonoBehaviour
 {
+    public Action OnTrigger;
+    
     public AudioSource audioSource;
     public int sampleDataLength = 1024;
     private float[] clipSampleData;
 
     private bool isTrigger = false;
     [SerializeField] private float border = 0.5f;
-    void Start() {
+
+    private float totalTime = 0f;
+    private float singleTime = 0f;
+
+    private bool isPlay = false;
+    private float _dealyTime { get => 60f / _BPM; }
+    [FormerlySerializedAs("BPM")] [SerializeField] private int _BPM = 120;
+    public int BPM { get => _BPM; }
+    private void Start() {
         clipSampleData = new float[sampleDataLength];
     }
 
-    void Update() {
-        
-        
-        if (Input.GetKeyDown(KeyCode.Space))
+    private bool isCheckVolume = false;
+
+    private void Update() 
+    {
+        if (Input.GetKeyDown(KeyCode.Z) && !audioSource.isPlaying)
         {
             audioSource.Play();
+            totalTime = 0f;
+            singleTime = 0f;
+            isTrigger = false;
+            isCheckVolume = true;
+        }
+        else if (Input.GetKeyDown(KeyCode.X) && !audioSource.isPlaying)
+        {
+            audioSource.Play();
+            totalTime = 0f;
+            isPlay = true;
+            WhileLoop();
         }
 
-        if (!audioSource.isPlaying)
-            return;
-        
-        audioSource.clip.GetData(clipSampleData, audioSource.timeSamples);
-        float loudness = 0f;
-        foreach (var sample in clipSampleData) {
-            loudness += Mathf.Abs(sample);
+        if (Input.GetKeyDown(KeyCode.Escape) && audioSource.isPlaying)
+        {
+            isPlay = false;
+            audioSource.Stop();
+            isCheckVolume = false;
         }
-        loudness /= sampleDataLength; // 計算平均振幅
+
+        
+        if (!audioSource.isPlaying )
+            return;
+        if(isCheckVolume)
+            CheckVolume();
+    }
+
+    private float GetLoudness
+    {
+        get
+        {
+            audioSource.clip.GetData(clipSampleData, audioSource.timeSamples);
+            float loudness = 0f;
+            foreach (var sample in clipSampleData) {
+                loudness += Mathf.Abs(sample);
+            }
+        
+            return loudness / sampleDataLength; // 計算平均振幅
+        }
+    }
+
+    private void CheckVolume()
+    {
+        float loudness = GetLoudness;
         bool result = loudness >= border;
-        if (result == isTrigger) return;
+        totalTime += Time.deltaTime;
+        if (result == isTrigger)
+        {
+            singleTime += Time.deltaTime;
+            return;
+        }
+        
         isTrigger = result;
-        if(isTrigger)
-            Debug.Log("音量強度: " + loudness);
+        if (isTrigger)
+        {
+            Debug.Log($"隨音量觸發，音量強度: {loudness}, 拍子間隔: {singleTime}, 總時間: {totalTime}" );
+            singleTime = 0f;
+            OnTrigger?.Invoke();
+        }
+    }
+
+    private void WhileLoop()
+    {
+        StartCoroutine(DelayTrigger());
+    }
+
+    private IEnumerator DelayTrigger()
+    {
+        while (audioSource.isPlaying && isPlay)
+        {
+            totalTime += _dealyTime;
+            Debug.Log($"隨延遲觸發，音量強度: {GetLoudness}, 拍子間隔: {_dealyTime}, 總時間: {totalTime}" );
+            OnTrigger?.Invoke();
+            yield return new WaitForSecondsRealtime(_dealyTime);
+        }
     }
 }
